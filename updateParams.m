7c6275596c6d6df6a6d075e893b25bf13777e853
rvtomas1 <89670105+rvtomas1@users.noreply.github.com>
2021-10-05 08:51:51 +0800

    Update updateParams.m

--- updateParams.m
+++ updateParams.m
@@ -1,8 +1,9 @@
 function [W, b] = updateParams(W, b, dW, db, LR)
 % this function updates the wights and biases using the computed gradients
 % dW and db.
-
-    W = W - LR*dW;
-    b = b - LR*db;
+    for j = 1:size(W,2)
+        W{j} = W{j} - LR*dW{j};
+        b{j} = b{j} - LR*db{j};
+    end
 end
 
