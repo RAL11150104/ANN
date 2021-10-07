09d8b0e12211cdc2af2d7800deb55be224ee4534
RLDejesus <91536460+RLDejesus@users.noreply.github.com>
2021-10-07 22:45:49 +0800

    Update Eval_ANN.m

--- Eval_ANN.m
+++ Eval_ANN.m
@@ -4,23 +4,23 @@
 % OUTPUT: Yo
 
 function [Yo] = Eval_ANN(W,b,X_test)
-    %% Initialization
+    %%Initialization
     [~,L] = size(W);
     Z = cell(1,L);
     A = cell(1,L);
     X_test = X_test./255; %Data Normalization
-    %% Forward-pass
+    %%Forward-pass
     for i = 1:L
         if i == 1
             Z{i} = X_test*W{1,i}.'+b{1,i}.';
-            A{i} = relu(Z{i});  %% 300x14
+            A{i} = relu(Z{i});  %%300x14
         elseif i == L
             Z{i} = A{i-1}*W{1,i};
             A{i} = softmax(Z{i}.'); % To make output into probabilities
-            Yo = A{L}.'; %% 300x3
+            Yo = A{L}.'; %%300x3
         else
             Z{i} = A{i-1}*W{1,i}.'+b{1,i}.'; 
-            A{i} = relu(Z{i}); %% 300x14
+            A{i} = relu(Z{i}); %%300x14
         end
     end
 end
